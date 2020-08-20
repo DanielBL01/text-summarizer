a51f94df9dd0dfa475b57823ff23f9fe67353d71
DanielBL01 <dblee0532@gmail.com>
2020-08-19 22:13:53 -0400

    Receive text input and summarize

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from textSummarizer import summary
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Starting the project'
+    if request.method == 'POST':
+        text = request.form['text']
+        final_summary = summary.call_summary(text)
+
+        return render_template('index.html', final_summary=final_summary)
+
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
